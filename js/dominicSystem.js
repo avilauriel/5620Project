500db023595f6d1409edb91e88603c24d0d5cba2
avilauriel <98366676+avilauriel@users.noreply.github.com>
2022-04-29 15:11:34 -0700

    new DS js for DS.html page

---
+++ js/dominicSystem.js
@@ -0,0 +1,464 @@
+
+const dominicSystemObjectArray = [
+    {
+        id: '00',
+        dsAbbreviation: "OO",
+        firstName: 'Olive',
+        lastName: 'Oyl',
+        action: 'eating',
+        object: 'spinach',
+        story: 'Olive Oyl is flirting with Popeye while eating spinach.',
+        tools: ['Memory Palace', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '01',
+        dsAbbreviation: "OA",
+        firstName: 'Neo',
+        lastName: 'The One',
+        action: 'stops',
+        object: 'bullets',
+        story: 'Neo dove into Mr. Smith after stoping the bullets. His hydrogen core exploded red in color',
+        tools: ['Memory Palace', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 78,
+            midterm: 85, 
+            final: 86
+        }
+    },{
+        id: '02',
+        dsAbbreviation: "OB",
+        firstName: 'Obi-Wan',
+        lastName: 'Kenobi',
+        action: 'swinging',
+        object: 'light-sabre',
+        story: 'His light-sabre pulls helium out of the sun',
+        tools: ['Memory Palace', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 88,
+            midterm: 83, 
+            final: 89
+        }
+    },{
+        id: '03',
+        dsAbbreviation: "OC",
+        firstName: 'C-3PO',
+        lastName: '',
+        action: 'badgering',
+        object: 'R2-D2',
+        story: 'C-3PO is sumo wrestling R2-D2 in a trench for a lithium fix!',
+        tools: ['Memory Palace', 'Deck of Cards'],
+        complete: false,
+        exception: false,
+        exams: { 
+            quiz: 88,
+            midterm: 83, 
+            final: 89
+        }
+    },{
+        id: '04',
+        dsAbbreviation: "OD",
+        firstName: 'Oscar',
+        lastName: 'de la Joya',
+        action: 'celebrates',
+        object: 'win',
+        story: 'Oscar de la Joya is celebrating his Olyinpic win.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '05',
+        dsAbbreviation: "OE",
+        firstName: 'Omar',
+        lastName: 'Epps',
+        action: 'diving',
+        object: 'second-base',
+        story: 'Omar dove was bored on second base.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '06',
+        dsAbbreviation: "OS",
+        firstName: 'O.J.',
+        lastName: 'Simpson',
+        action: 'driving',
+        object: 'bronco',
+        story: 'OJ Simpson and his bronco incinerate into carbon.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '07',
+        dsAbbreviation: "OG",
+        firstName: 'James',
+        lastName: 'Bond',
+        action: 'crashes',
+        object: 'DB9',
+        story: 'James Bond used Nitorgren in the DB9 and flew through the sky.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: true,
+        exams: { 
+            quiz: 89,
+            midterm: 94, 
+            final: 88
+        }
+    },{
+        id: '08',
+        dsAbbreviation: "OH",
+        firstName: 'Oliver',
+        lastName: 'Hardy',
+        action: 'laughing',
+        object: 'hysterically',
+        story: 'Oliver Hardy is laughing hysterically on the sofa as he gasped for oxygen.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '09',
+        dsAbbreviation: "ON",
+        firstName: 'Olivia',
+        lastName: 'Newton-John',
+        action: 'flirting',
+        object: 'Danny Zuko',
+        story: 'In the bathroom, ONJ grabbed flourine then rinsed her face with soap.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '10',
+        dsAbbreviation: "AO",
+        firstName: 'Ashley',
+        lastName: 'Alsen',
+        action: 'falls-off',
+        object: 'bed',
+        story: 'Ashley is admiring her neon sign in Richmond, VA.',
+        tools: [ 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '11',
+        dsAbbreviation: "AA",
+        firstName: 'Alonso',
+        lastName: 'Avila',
+        action: 'smoking',
+        object: 'meats',
+        story: 'Aloson Avila is adding the perfect amount of sodium to the the meat.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '12',
+        dsAbbreviation: "AB",
+        firstName: 'Antonio',
+        lastName: 'Banderas',
+        action: 'makes',
+        object: 'sword',
+        story: 'Antonio Banderas made a sword os magnesium so thin it looked NoRMal.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '13',
+        dsAbbreviation: "AC",
+        firstName: 'Al',
+        lastName: 'capone',
+        action: 'bribes',
+        object: 'judge',
+        story: 'Al Capone had the foresight to build an aluminum dam in providence, RI.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '14',
+        dsAbbreviation: "AD",
+        firstName: 'Albus',
+        lastName: 'Dumbledore',
+        action: 'points',
+        object: 'wand',
+        story: 'Albus Dumbledore, battleing with Voldermot, points a silicon wand through the door.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '15',
+        dsAbbreviation: "AE",
+        firstName: 'Amanda',
+        lastName: 'Emery',
+        action: 'skinny-dipping',
+        object: 'jacuzzi',
+        story: '.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: false,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '16',
+        dsAbbreviation: "AS",
+        firstName: 'Arnold',
+        lastName: 'Scwarzenegger',
+        action: 'shoots',
+        object: 'T-1000',
+        story: 'Arnolds watch is made out of sulfur; and it stinks.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '17',
+        dsAbbreviation: "AG",
+        firstName: 'Ariana',
+        lastName: 'Grande',
+        action: 'swimming',
+        object: 'pool',
+        story: 'Ariana Grande is swimming in her chlorine tears filled pool.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '18',
+        dsAbbreviation: "AH",
+        firstName: 'Anna',
+        lastName: 'Hernandez',
+        action: '',
+        object: '',
+        story: 'Anna Hernandez is something... Argon',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: false,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '19',
+        dsAbbreviation: "AN",
+        firstName: 'Alfred E.',
+        lastName: 'Newman',
+        action: 'smiling',
+        object: 'teeth',
+        story: 'Alfred E. Newmas twop twin-teeth are holding a postassium filled banana.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '20',
+        dsAbbreviation: "BO",
+        firstName: 'Bo',
+        lastName: 'Jackson',
+        action: 'knows',
+        object: 'football',
+        story: "Bo Jackson's calcium filled nose smelles like roses.",
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '21',
+        dsAbbreviation: "BA",
+        firstName: 'Ben',
+        lastName: 'Affleck',
+        action: 'pricked',
+        object: 'needle',
+        story: 'UBen Afflec felt the needle all the way to his Scandium.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '22',
+        dsAbbreviation: "BB",
+        firstName: 'Bugs',
+        lastName: 'Bunny',
+        action: 'leads',
+        object: 'opera',
+        story: 'Bugs Bunny is yanking on a titanium Onion.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '23',
+        dsAbbreviation: "BC",
+        firstName: 'Bill',
+        lastName: 'Clinton',
+        action: 'smells',
+        object: 'cigar',
+        story: 'After Monica, Bill smelled the cigar with Vana; Vanadium.',
+        tools: ['Pi', 'Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '24',
+        dsAbbreviation: "BD",
+        firstName: 'Bo',
+        lastName: 'Derek',
+        action: 'running',
+        object: 'beach',
+        story: 'Bo Derecks har-dredlocks are nade out of chromium.',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: true,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },{
+        id: '25',
+        dsAbbreviation: "BE",
+        firstName: 'Billy',
+        lastName: 'Elliot',
+        action: 'dancing',
+        object: 'Ballet',
+        story: '',
+        tools: ['Memory Palace', 'Journey method', 'Deck of Cards'],
+        complete: false,
+        exception: false,
+        exams: { 
+            quiz: 87,
+            midterm: 92, 
+            final: 89
+        }
+    },
+
+]
+
+// Average .  This average is not working in the array.
+// const avg = (dominicSystemObjectArray.exams.quiz + dominicSystemObjectArray.exams.midterm + dominicSystemObjectArray.exams.final);
+
+console.log("First array typing in js. ", dominicSystemObjectArray);
+
+
+// The Creation of the Dominic System Object Array.
+// This should show the object array with the iten names followed by the values
+let progressionDominicSystemArray = [...new Map(dominicSystemObjectArray.map((item) => [item["firstName"], item])).values()]
+
+
+// Shows the lable name and value for each item. Currently therere are (6)
+console.log("New Map Array ", progressionDominicSystemArray)
+
+// Creating a test pregression on the DS and making test item id into the key Map(6)
+let test_progressionDominicSystemArray_map = dominicSystemObjectArray.map((item) => [item["id"], item])
+
+//  Shows the list as an array with the id, and the details for each entry
+console.log("The test_progressionDominicSystemArray_map shows: ", test_progressionDominicSystemArray_map);
+
+// testing the newMap object array contains the values in each id value
+let test_progressionDominicSystemArray_newMap = new Map(test_progressionDominicSystemArray_map);
+
+console.log(test_progressionDominicSystemArray_newMap);
+
+// Create a list of all the keys and make an iterator. Single value as the key MapIterator
+let test_uniqueObjArray_newMapKeys = test_progressionDominicSystemArray_newMap.keys();
+
+console.log(test_uniqueObjArray_newMapKeys);
+
+
+// Create a list of all the values and make an iterator for each one. All the fields with name PAO
+let test_uniqueObjArray_newMapValues = test_progressionDominicSystemArray_newMap.values();
+
+// These objects shows the DS entries with the details.  MapIterator
+console.log(test_uniqueObjArray_newMapValues);
+
+// A spread operator in a new array with backets [...] and dots.  (6)
+// This made the onject array into another array from the test_uniqueObjArray_newMapValues
+let test_uniqueObjArray_newMapValuesAsArray = [...test_uniqueObjArray_newMapValues];
+
+console.log("DS Object in a new array ", test_uniqueObjArray_newMapValuesAsArray);
